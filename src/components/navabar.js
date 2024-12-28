8d9645b39e7f50edd9033a8cd8eb6397fcde7902
Nikhil-Dhande <ndhande123@gmail.com>
2024-12-28 23:51:03 +0530

    navbar merge fix

--- src/components/navabar.js
+++ src/components/navabar.js
@@ -4,8 +4,6 @@ export default function Navbar() {
   return (
     <nav className="bg-transparent fixed w-full z-20 top-3 left-0">
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4 border-2 border-[#aacde2] rounded-full">
-    <nav className="bg-transparent fixed w-full z-20 top-0 left-0 border-b border-gray-100 dark:border-gray-700">
-      <div className="max-w-screen-xl mx-auto p-4 flex items-center justify-between">
         {/* Logo */}
         <Link
           href="/"
@@ -14,12 +12,10 @@ export default function Navbar() {
           <img
             src="/logo.jpeg" // Place your logo in the `public` folder of your Next.js project
             alt="Logo"
-            className="h-8"
-            className="h-20 rounded-full w-35"
+            
+            className="h-10 rounded-full w-35"
           />
-          <span className="self-center text-2xl font-extrabold whitespace-nowrap text-white">
-            Flowbite
-          </span>
+         
           {/* <span className="self-center text-3xl font-extrabold whitespace-nowrap text-gray-800 dark:text-white">
             HealthAI
           </span> */}
@@ -30,28 +26,24 @@ export default function Navbar() {
           <Link
             href="/"
             className="text-xl text-white hover:text-blue-400 font-semibold"
-            className="text-gray-900 hover:text-blue-700 dark:text-white dark:hover:text-blue-500"
           >
             Home
           </Link>
           <Link
             href="/about"
             className="text-xl text-white hover:text-blue-400 font-semibold"
-            className="text-gray-900 hover:text-blue-700 dark:text-white dark:hover:text-blue-500"
           >
             About
           </Link>
           <Link
             href="/services"
             className="text-xl text-white hover:text-blue-400 font-semibold"
-            className="text-gray-900 hover:text-blue-700 dark:text-white dark:hover:text-blue-500"
           >
             Services
           </Link>
           <Link
             href="/contact"
             className="text-xl text-white hover:text-blue-400 font-semibold"
-            className="text-gray-900 hover:text-blue-700 dark:text-white dark:hover:text-blue-500"
           >
             Contact
           </Link>
@@ -65,12 +57,7 @@ export default function Navbar() {
           >
             Get Started
           </button>
-        <button
-          type="button"
-          className="text-xl text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg px-6 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Get Started
-        </button>
+        
 
         {/* Mobile Menu Toggle */}
         <button
